2f2eed68a7d8dcc04204651d0a6e08eda732dd11
sumey <sumeyye72.sa@gmail.com>
2021-12-06 22:36:45 +0300

    Food Drop Game

---
+++ Assets/Scripts/character.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class character : MonoBehaviour
+{
+    public AudioSource eatSound;
+    public float velocity;
+    public Transform foodd;
+    int score;
+    public TextMeshProUGUI score_Text;
+
+    void Update()
+    {
+        score_Text.text = "Score: " + score;
+        movementCharacter();
+    }
+
+    void movementCharacter(){
+
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            transform.Translate(-velocity * Time.deltaTime,0,0);
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            transform.Translate(velocity * Time.deltaTime,0,0);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+        float xPosition = Random.Range(-5f,5f);
+
+        if(col.gameObject.tag=="food"){
+            foodd.position = new Vector3(xPosition,7f,0f);
+            eatSound.Play();
+            score+=1;
+        }
+    
+    } 
+} 
+
---
+++ Assets/Scripts/pomegranate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pomegranate : MonoBehaviour
+{   
+     public Transform food;
+    void OnCollisionEnter2D(Collision2D collision){
+        float xPosition = Random.Range(-5f,5f);
+
+        if(collision.gameObject.tag=="ground"){
+           food.position = new Vector3(xPosition,7f,0f);        
+        }
+    }
+}
